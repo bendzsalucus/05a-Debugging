461ce4a25ef9c6ce2087cd8324b56a07ec49826f
bendzslt <bendzslt@rose-hulman.edu>
2018-09-10 09:47:35 -0400

    WIP.

--- src/m1.py
+++ src/m1.py
@@ -2,8 +2,8 @@
 This module lets you practice DEBUGGING when RUN-TIME EXCEPTIONS occur.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Lucus Bendzsa.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -173,10 +173,9 @@ def broken_1(circle, window):
       :type window: rg.RoseWindow
     """
     circle.attach_to(window)
-    circle2 = rg.Circle(circle.radius * 2)
+    circle2 = rg.Circle(circle.center, circle.radius * 2)
     circle2.attach_to(window)
     window.render()
-
 
 # ----------------------------------------------------------------------
 # TODO: 4. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
